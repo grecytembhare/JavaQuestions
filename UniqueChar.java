39936940a9022543baa8a22f10c5d05da49432a1
Grecy Tembhare <tembharegrecy@gmail.com>
2023-10-16 21:27:59 +0530

    for find first unique char.

---
+++ UniqueChar.java
@@ -0,0 +1,43 @@
+
+/* Date: 16-10-2023. Write a Java program to identify and print the first unique character in a given string. */
+
+import java.util.Scanner;
+
+public class UniqueChar {
+    
+    public static char firstUniqueCharacter(String find) {
+        int[] charCount = new int[26]; 
+
+        // Count the frequency of each character in the string
+        for (int i = 0; i < find.length(); i++) {
+            charCount[find.charAt(i) - 'a']++;
+        }
+
+        // Iterate through the string to find the first unique character
+        for (int i = 0; i < find.length(); i++) {
+            if (charCount[find.charAt(i) - 'a'] == 1) {
+                return find.charAt(i);
+            }
+        }
+
+
+        // If no unique character is found, return a default value
+         return 'N' ;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter any String : ");
+        Scanner userinput = new Scanner(System.in);
+        String input = userinput.next();
+        
+        char firstUnique = firstUniqueCharacter(input);
+         
+        userinput.close();
+
+        if (firstUnique != 'N') {
+            System.out.println("The first unique character is: " + firstUnique);
+        } else {
+            System.out.println("No unique character found in the input string.");
+        }
+    }
+}
