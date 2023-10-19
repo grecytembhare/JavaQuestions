13e339a177e859eaf67131ca2ffd1311f1131baa
Grecy Tembhare <tembharegrecy@gmail.com>
2023-10-19 21:06:40 +0530

    palindrome

---
+++ Palindrome.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner pali = new Scanner(System.in);
+        System.out.print("Enter a word or phrase: ");
+        String input = pali.nextLine();
+        pali.close();
+
+        if (isPalindrome(input)) {
+            System.out.println("It is a palindrome.");
+        } else {
+            System.out.println("It is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(String input) {
+        // Remove spaces and convert to lowercase for case-insensitive comparison
+        input = input.replaceAll("\\s", "").toLowerCase();
+
+        int left = 0;
+        int right = input.length() - 1;
+
+        while (left < right) {
+            if (input.charAt(left) != input.charAt(right)) {
+                return false; // Not a palindrome
+            }
+            left++;
+            right--;
+        }
+
+        return true; // It's a palindrome
+    }
+}
